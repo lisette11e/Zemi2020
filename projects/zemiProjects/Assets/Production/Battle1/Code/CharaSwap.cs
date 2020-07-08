cd1aae6c4aa6c58759dee92e4fe95772f0e3ea66
lisette11e <18mv083@st.nuas.ac.jp>
2020-07-08 17:02:58 +0900

    キャラチェンジ仮実装
    一応基盤だけ実装しました。多分まだしっかり動かないと思いますので、今後まだまだ修正入ると思ってもらっておｋです。

---
+++ projects/zemiProjects/Assets/Production/Battle1/Code/CharaSwap.cs
@@ -0,0 +1,18 @@
+/* キャラクタースワップ
+*/
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharaSwap : MonoBehaviour
+{
+  GameObject director;
+  GameDirector gd;
+  void Start(){
+    director = GameObject.Find("GameDirector");
+    gd = director.GetComponent<GameDirector>();
+  }
+      public void Swap(){
+
+    }
+}
--- projects/zemiProjects/Assets/Production/Battle1/Code/GameDirector.cs
+++ projects/zemiProjects/Assets/Production/Battle1/Code/GameDirector.cs
@@ -30,6 +30,18 @@ public class GameDirector : MonoBehaviour
   //各種ステータス宣言
   public int AutoHealCount = 0;
   public int ManualHealCount = 0;
+  public int CurrentPlayerHp;
+  public int CurrentPlayerLv ;
+  public int CurrentPlayerAttack;
+  public int CurrentPlayerabilitycount;
+  public int StandbyPlayerHp;
+  public int StandbyPlayerLv;
+  public int StandbyPlayerAttack;
+  public int StandbyPlayerabilitycount;
+  public int SwapHp;
+  public int SwapLv;
+  public int SwapAttack;
+  public int Swapabilitycount;
     GameObject hpGauge;
     void Start()
     {
@@ -45,6 +57,21 @@ public class GameDirector : MonoBehaviour
         Yuh.Lv = 1;
         Yuh.Attack = 100;
         Yuh.abilitycount = 0;
+
+        CurrentPlayerHp = Lilia.Hp;
+        CurrentPlayerLv = Lilia.Lv;
+        CurrentPlayerAttack = Lilia.Attack;
+        CurrentPlayerabilitycount = Lilia.abilitycount;
+        StandbyPlayerHp = Yuh.Hp;
+        StandbyPlayerLv = Yuh.Lv;
+        StandbyPlayerAttack = Yuh.Attack;
+        StandbyPlayerabilitycount = Yuh.abilitycount;
+
+
+        //最初はリリアが出撃するようにする（仕様変更あれば対応可）
+
+
+
 
         //敵・ざこいの（小）
         Enemy EnemyMobSmall = new Enemy();
@@ -87,4 +114,22 @@ public class GameDirector : MonoBehaviour
         this.hpGauge.GetComponent<Image> ().fillAmount -= 0.05f;
 
       }
-}
+
+    //プレイヤー入れ替え
+    public void PlayerSwap(){
+      SwapHp = CurrentPlayerHp;
+      SwapLv = CurrentPlayerLv;
+      SwapAttack = CurrentPlayerAttack;
+      Swapabilitycount = CurrentPlayerabilitycount;
+
+      CurrentPlayerHp = StandbyPlayerHp;
+      CurrentPlayerLv = StandbyPlayerLv;
+      CurrentPlayerAttack = StandbyPlayerAttack;
+      CurrentPlayerabilitycount = StandbyPlayerabilitycount;
+
+      StandbyPlayerHp = SwapHp;
+      StandbyPlayerLv = SwapLv;
+      StandbyPlayerAttack = SwapAttack;
+      StandbyPlayerabilitycount = Swapabilitycount;
+    }
+  }
