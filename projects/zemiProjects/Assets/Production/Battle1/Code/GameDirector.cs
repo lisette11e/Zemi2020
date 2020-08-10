e5c21bb37a23cc10f7fed110086b8b2a38c66908
lisette11e <18mv083@st.nuas.ac.jp>
2020-08-10 22:16:38 +0900

    設計変更
    シングルトン設計に伴う設定の変更を行いました。

--- projects/zemiProjects/Assets/Production/Battle1/Code/GameDirector.cs
+++ projects/zemiProjects/Assets/Production/Battle1/Code/GameDirector.cs
@@ -29,55 +29,12 @@ public class GameDirector : MonoBehaviour {
     }
   }
   //各種ステータス宣言
-  public int AutoHealCount = 0;
-  public int ManualHealCount = 0;
-  public int CurrentPlayerHp;
-  public int CurrentPlayerLv;
-  public int CurrentPlayerAttack;
-  public int CurrentPlayerabilitycount;
-  public int StandbyPlayerHp;
-  public int StandbyPlayerLv;
-  public int StandbyPlayerAttack;
-  public int StandbyPlayerabilitycount;
-  public int SwapHp;
-  public int SwapLv;
-  public int SwapAttack;
-  public int Swapabilitycount;
-  public int LiliaHp;
-  public int LiliaLv;
-  public int LiliaAttack;
-  public int LiliaAbilityCount;
-  public int YuhHp;
-  public int YuhLv;
-  public int YuhAttack;
-  public int YuhAbilityCount;
-  public bool ToChange = false;
   public float SPmoveTimer = 0.0f;
   public float SPstandbyTimer = 0.0f;
   public bool ToSpecialAttack = false;
   public float bgspeed = -0.05f;
   GameObject hpGauge;
   void Start () {
-    //リリア
-    LiliaHp = 500;
-    LiliaLv = 1;
-    LiliaAttack = 120;
-    LiliaAbilityCount = 0;
-
-    //ユウ
-    YuhHp = 400;
-    YuhLv = 1;
-    YuhAttack = 100;
-    YuhAbilityCount = 0;
-
-    CurrentPlayerHp = LiliaHp;
-    CurrentPlayerLv = LiliaLv;
-    CurrentPlayerAttack = LiliaAttack;
-    CurrentPlayerabilitycount = LiliaAbilityCount;
-    StandbyPlayerHp = YuhHp;
-    StandbyPlayerLv = YuhLv;
-    StandbyPlayerAttack = YuhAttack;
-    StandbyPlayerabilitycount = YuhAbilityCount;
 
     /* 仮置です
     //敵・ざこいの（小）
@@ -106,44 +63,6 @@ public class GameDirector : MonoBehaviour {
 
   }
 
-  //被弾処理
-  public void DecreaseHp (int enemyattack) {
-    //被弾時にコンボ値を変更できるようにする
-    float currentHp = 0.0f;
-    float MaxHp = 0.0f;
-    float hpGaugeFill = 0.0f;
-    CurrentPlayerHp -= enemyattack;
-    currentHp = (float) CurrentPlayerHp;
-    if (ToChange == false) {
-      MaxHp = LiliaHp;
-    } else {
-      MaxHp = YuhHp;
-    }
-
-    hpGaugeFill = currentHp / MaxHp;
-    this.hpGauge.GetComponent<Image> ().fillAmount = hpGaugeFill;
-
-    Debug.Log (currentHp);
-
-  }
-
-  //プレイヤー入れ替え
-  public void PlayerSwap () {
-    SwapHp = CurrentPlayerHp;
-    SwapLv = CurrentPlayerLv;
-    SwapAttack = CurrentPlayerAttack;
-    Swapabilitycount = CurrentPlayerabilitycount;
-
-    CurrentPlayerHp = StandbyPlayerHp;
-    CurrentPlayerLv = StandbyPlayerLv;
-    CurrentPlayerAttack = StandbyPlayerAttack;
-    CurrentPlayerabilitycount = StandbyPlayerabilitycount;
-
-    StandbyPlayerHp = SwapHp;
-    StandbyPlayerLv = SwapLv;
-    StandbyPlayerAttack = SwapAttack;
-    StandbyPlayerabilitycount = Swapabilitycount;
-  }
 
   //スペシャルムーブ発動
   public void runSpecialMove () {
