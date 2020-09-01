0e3adeefbe87e5f1884f785b8b35a8fbb3f52a2b
lisette11e <18mv083@st.nuas.ac.jp>
2020-09-01 18:00:31 +0900

    リリアのアビリティ実装（#77）

---
+++ projects/zemiProjects/Assets/Production/Battle1/Code/GenLiliaMob.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenLiliaMob : MonoBehaviour {
+    public GameObject lilia;
+    public GameObject LiliaSpMob;
+    public Transform liliapos;
+    // Start is called before the first frame update
+    void Start () {
+        lilia = GameObject.Find ("stMychara");
+        liliapos = lilia.GetComponent<Transform> ();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Vector3 pos = liliapos.position;
+        if (Input.GetMouseButtonDown (0) && PlayerManager.Instance.LiliaAbilityCount <= 2) {
+            Instantiate (LiliaSpMob, new Vector3 (pos.x + 1.0f, pos.y, 0.0f), Quaternion.identity);
+            Instantiate (LiliaSpMob, new Vector3 (pos.x - 1.0f, pos.y, 0.0f), Quaternion.identity);
+        }
+    }
+}
