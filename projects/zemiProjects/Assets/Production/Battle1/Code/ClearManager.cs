a008aa5ec50a29329e607b9a2855fbecf0999c4f
hamasakieiji <69409835+hamasakieiji@users.noreply.github.com>
2020-09-04 16:02:44 +0900

    200904 中ボスif
    中ボスif実装途中

---
+++ projects/zemiProjects/Assets/Production/Battle1/Code/ClearManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearManager : MonoBehaviour
+{
+    TypefaceAnimator anim; // 変数の定義
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = this.gameObject.GetComponent<TypefaceAnimator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (EnemyMobMediumHp = 0;
+
+        {
+            anim.Play();
+        }
+    }
+}
