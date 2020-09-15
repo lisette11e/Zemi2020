cc97a4a5225174dc1b9f60b05fddb60617a193b5
lisette11e <18mv083@st.nuas.ac.jp>
2020-09-15 17:28:41 +0900

    必殺モード実装中（#14）
    必殺モード実装中です。
    背景を早くするのと、自弾のエフェクトが大きくなるところまでいきました。
    ※開発段階丁度いいところまで行ったので、ひとまずここで定期報告とします

--- projects/zemiProjects/Assets/Production/Battle1/Code/BackGround.cs
+++ projects/zemiProjects/Assets/Production/Battle1/Code/BackGround.cs
@@ -2,13 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BackGround : MonoBehaviour {
-	void start () { }
-	void Update () {
+public class BackGround : MonoBehaviour
+{
+    void start() { }
+    void Update()
+    {
+        if (GameDirector.Instance.ToSpecialAttack == true)
+        {
+            transform.Translate(0, -0.1f, 0);
+        }
+        else
+        {
+            transform.Translate(0, -0.05f, 0);
+        }
 
-		transform.Translate (0, -0.05f, 0);
-		if (transform.position.y < -12.0f) {
-			transform.position = new Vector3 (0, 12.0f, 0);
-		}
-	}
+        if (transform.position.y < -12.0f)
+        {
+            transform.position = new Vector3(0, 12.0f, 0);
+        }
+    }
 }
