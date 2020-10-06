f60feb34f3a5ee1a1174ceca5faf5efbab0a38bd
lisette11e <18mv083@st.nuas.ac.jp>
2020-10-06 22:55:23 +0900

    add camera support（WIP/#114）
    制作中。
    用事ができて家を出ないといけなくなったので、忘れないうちにpushしときます。
    
    ※BGGUIが曲者で何故か動かなくなった上にキャラが隠れてしまう。
    ほかはなんとかなりそう。

--- projects/zemiProjects/Assets/Production/Battle1/Code/BackGround.cs
+++ projects/zemiProjects/Assets/Production/Battle1/Code/BackGround.cs
@@ -1,24 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BackGround : MonoBehaviour
 {
-    void start() { }
+    RectTransform BG;
+    void start() { 
+        BG = GetComponent<RectTransform>();
+    }
     void Update()
     {
         if (GameDirector.Instance.ToSpecialAttack == true)
         {
-            transform.Translate(0, -0.1f, 0);
+            BG.Translate (0, -20.0f, 0);
         }
         else
         {
-            transform.Translate(0, -0.05f, 0);
+           BG.Translate (0, -10.0f, 0);
         }
 
-        if (transform.position.y < -12.0f)
+        if (BG.localPosition.y < -2000.0f)
         {
-            transform.position = new Vector3(0, 12.0f, 0);
+            BG.localPosition = new Vector3(0, 2000.0f, 0);
         }
     }
 }
