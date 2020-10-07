d1bfacf7897d295eaf6f2bc1636ff182e99286bb
lisette11e <18mv083@st.nuas.ac.jp>
2020-10-07 10:21:26 +0900

    bugfix for #144
    bgが動かなくなる不具合修正。
    及び、雑魚キャラのマネージャーに、コンボ加算が二重に行われているところを見つけたので修正。

--- projects/zemiProjects/Assets/Production/Battle1/Code/BulletControll.cs
+++ projects/zemiProjects/Assets/Production/Battle1/Code/BulletControll.cs
@@ -6,52 +6,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletControll : MonoBehaviour {
+public class BulletControll : MonoBehaviour
+{
     public GameObject explosionPrefab;
     public GameObject targetEnemy;
-    void Start () { }
+    void Start() { }
 
     //ホーミング用
     public float diffusionAngle = 0.5f;
     public float bulletSpeed = 0.05f;
 
-    void Update () {
+    void Update()
+    {
         //　自機弾の発射方向
-        transform.Translate (0, 0.05f, 0);
+        transform.Translate(0, 0.05f, 0);
 
-        if (!GetComponent<Renderer> ().isVisible) {
-            Destroy (gameObject);
+        if (!GetComponent<Renderer>().isVisible)
+        {
+            Destroy(gameObject);
         }
     }
 
-    void OnTriggerEnter2D (Collider2D collision) {
+    void OnTriggerEnter2D(Collider2D collision)
+    {
         //接触したオブジェクトのHPをへらす
         targetEnemy = collision.gameObject;
         int Combo = ScoreManager.Instance.CurrentCombo;
         //当たったオブジェクトを調べる（もうちょっと良いコードありそう）
-        switch (collision.gameObject.tag) {
-            case "SmallEnemyMob":
-                Destroy (gameObject);
-                targetEnemy.GetComponent<SmallEnemyManager> ().DecreaseHp ();
-                break;
-            case "BigEnemyMob":
-                Destroy (gameObject);
-                break;
-            case "MediumEnemy":
-                Destroy (gameObject);
-                targetEnemy.GetComponent<MediumEnemyManager> ().DecreaseHp ();
-                break;
-            case "BigBoss":
-                Destroy (gameObject);
-                break;
-            case "Player":
-                break;
-            default:
-                Destroy (targetEnemy);
-                break;
+        if (collision.gameObject.tag != "Player")
+        {
+            switch (collision.gameObject.tag)
+            {
+                case "SmallEnemyMob":
+                    Destroy(gameObject);
+                    targetEnemy.GetComponent<SmallEnemyManager>().DecreaseHp();
+                    break;
+                case "BigEnemyMob":
+                    Destroy(gameObject);
+                    break;
+                case "MediumEnemy":
+                    Destroy(gameObject);
+                    targetEnemy.GetComponent<MediumEnemyManager>().DecreaseHp();
+                    break;
+                case "BigBoss":
+                    Destroy(gameObject);
+                    break;
+                default:
+                    Destroy(targetEnemy);
+                    break;
+            }
+            double scrtmp = 100 * (Combo + 1) * 0.01;
+            int add = (int)scrtmp;
+            ScoreManager.Instance.AddScore(add);
+            Debug.Log(collision.gameObject.name);
         }
-        double scrtmp = 100 * (Combo + 1) * 0.01;
-        int add = (int) scrtmp;
-        ScoreManager.Instance.AddScore (add);
     }
 }
